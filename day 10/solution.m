f7a8dbd918ce6a13a7e70bf070432ce93b26329f
Vinay Krishna Nelli <vinaykrishna064@gmail.com>
2022-12-10 19:50:31 +1300

    day 10 added

---
+++ day 10/solution.m
@@ -0,0 +1,40 @@
+clear all
+clc
+
+S = readlines("input.txt").replace(["noop","addx"],["1 0","2"]).split(" ").double();
+
+signal = [1];
+for i = 1:size(S,1)
+    signal =[signal; 0];
+    if S(i,1) == 2   
+        signal = [signal; S(i,2)];
+    end
+end
+
+signal = cumsum(signal); %signal
+
+%% part 1
+end_L = floor((numel(signal)-20)/40);
+locs = 20 + [0, 40*(1:end_L)]';
+part_1 = sum(signal(locs).*locs)
+
+%% part 2
+
+screen = zeros(6,40);
+
+for i = 1:numel(signal)
+    row = floor(i/40);
+    col = i - row*40;
+    if abs(signal(i)+1 - col) <= 1
+        screen(row+1,col) = 1;
+    end
+end
+
+figure
+imshow(imresize(1 - screen,10))
+title('part 2 - CRT display')
+exportgraphics(gcf,'CRT.jpeg','Resolution',1200)
+
+
+
+
