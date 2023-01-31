6a78c6b65cb07c05a60c85c8e1518e55b7df81f6
Vinay Krishna Nelli <vinaykrishna064@gmail.com>
2023-01-31 19:56:54 +1300

    AOC 16 day 13 A* method

--- AOC2016/day 13/solution.py
+++ AOC2016/day 13/solution.py
@@ -1,39 +1,86 @@
 import copy
 import numpy as np
-from matplotlib import pyplot as plt
+from queue import PriorityQueue
 inpt = 1352 #input, to be added to base equation for wall
 
 def wall(loc):
     [x,y] = loc
     func =  bin(x**2 + 3*x + 2*x*y + y + y**2 + 1352)
     if func.count('1')%2 == 1:
-        return 0.5
+        return 1
     else:
         return 0
-
-def crawl(loc,visits,dist=0,m_dist=1000):
-    dirs = [[1,0],[0,-1],[0,1],[-1,0]]
-    visits[loc[0],loc[1]] = 1
-    if loc == [31,39]:
-        if dist < m_dist:
-            plt.rcParams["figure.figsize"] = [7.50, 3.50]
-            plt.rcParams["figure.autolayout"] = True
-            plt.gray()
-            plt.imshow(visits)
-            plt.show()
-            return dist
-    for i in range(4):
-        nxt = [loc[0] + dirs[i][0],loc[1] + dirs[i][1]]
-        if nxt[0] >= 0 and nxt[0] >= 1 :
-            if visits[nxt[0],nxt[1]] == 0:
-                m_dist = crawl(nxt,visits,dist+1,m_dist)
-    return m_dist
-
-
+def man_dist(loc,goal):
+    return abs(loc[0]-goal[0]) + abs(loc[1]-goal[1])
+def in_maze(loc,sz):
+    if loc[0] >= 0 and loc[1] >= 0 and loc[0] < sz[0] and loc[1] < sz[1]:
+        return 1
+    else:
+        return 0
 [sx,sy] = [100,100]
-visits = np.zeros((sx,sy))
+
+maze = np.zeros((sx,sy))
+
+
 for i in range(sx):
     for j in range(sy):
-        visits[i,j] = wall([i,j])
+        maze[i,j] = wall([i,j])
 
-print(crawl([1,1],visits))
+def A_star(start,goal,maze,sz):
+    g_cost = np.zeros((sz[0],sz[1]))
+    f_cost = np.zeros((sz[0],sz[1]))
+    for i in range(sz[0]):
+        for j in range(sz[1]):
+            g_cost[i,j] = float('inf')
+            f_cost[i,j] = float('inf')
+    g_cost[start[0],start[1]] = 0
+    f_cost[start[0],start[1]] = man_dist(start,goal)
+    dirs = [[-1,0],[0,1],[1,0],[0,-1]]
+    open = PriorityQueue()
+    open.put((f_cost[start[0],start[1]],man_dist(start,goal),start))
+    rvs_path = {}
+    while not open.empty(): #this checks for no path
+        Loc = open.get()[2]
+        if Loc[0] == goal[0] and Loc[1] == goal[1]:
+            break
+        for i in range(4):
+            nxt_Loc = [Loc[0]+dirs[i][0],Loc[1]+dirs[i][1]]
+            if  in_maze(nxt_Loc,sz) == 1 and maze[nxt_Loc[0],nxt_Loc[1]] == 0 :
+                temp_gC = g_cost[Loc[0],Loc[1]] + 1
+                temp_fC = temp_gC  + man_dist(nxt_Loc,goal)
+                if temp_fC < f_cost[nxt_Loc[0],nxt_Loc[1]]:
+                    g_cost[nxt_Loc[0],nxt_Loc[1]] = temp_gC
+                    f_cost[nxt_Loc[0],nxt_Loc[1]] = temp_fC
+                    open.put((temp_fC,man_dist(nxt_Loc,goal),nxt_Loc))
+                    rvs_path[tuple(nxt_Loc)] = tuple(Loc)
+    fwd_path = {}
+    
+    try:
+        key = tuple(goal)
+        while key!=tuple(start):
+            fwd_path[rvs_path[key]] = key
+            key = rvs_path[key]
+    except:
+        pass
+    return len(fwd_path)
+
+start = [1,1]
+goal = [31,39]
+print('part_1 = ',A_star(start,goal,maze,[sx,sy]))
+
+
+count = 1 #include start
+
+for i in range(sx):
+    for j in range(sy):
+        dist = A_star(start,[i,j],maze,[sx,sy])
+        if dist > 0 and dist <= 50:
+            count += 1
+
+print('part_2 = ',count)
+
+
+
+
+
+
