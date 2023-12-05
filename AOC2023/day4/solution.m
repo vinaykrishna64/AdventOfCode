9fb00f0c54a04f0ec412d6e75659b77d0d5eed49
Vinay Nelli <vinaykrishna064@gmail.com>
2023-12-06 07:25:15 +1300

    AOC23 day 5 up and day 4 clean

--- AOC2023/day4/solution.m
+++ AOC2023/day4/solution.m
@@ -1,28 +1,16 @@
 clear all
 filename    = 'input.txt';
 S           = readlines(filename);
-S           = strtrim(S.split(':'));
-cards       = S(:,2);
-
+winners     = str2double(strtrim(S.extractBetween(':','|')).replace('  ',' 0').split(' '));
+numbers     = str2double(strtrim(S.extractAfter('|')).replace('  ',' 0').split(' '));
 %% part 1
-for idx = 1:size(S,1)
-    card = strtrim(cards(idx).split('|'));
-    winners = str2double(card(1).split(' '));
-    numbers = str2double(card(2).split(' '));
-    matches(idx) = numel(intersect(winners,numbers));
-end
+matches = arrayfun(@(x) numel(intersect(winners(x,:),numbers(x,:))),1:numel(S));
 score = 2.^(matches-1);
 score(score <1 ) = 0;
-
 fprintf('part 1 = %d\n',sum(score))
 %% part 2
-scratched = 1;
-copies    = ones([numel(cards),1]);
-for idx = 1:numel(cards)
-    if matches(idx)
-        st_cpy = min(idx+1,numel(cards));
-        en_cpy = min(idx+matches(idx),numel(cards));
-        copies(st_cpy:en_cpy) = copies(st_cpy:en_cpy)+copies(idx);
-    end
+copies    = ones([numel(S),1]);
+for idx = 1:numel(S)
+    copies(idx+1:idx+matches(idx)) = copies(idx+1:idx+matches(idx))+copies(idx);
 end
 fprintf('part 1 = %d\n',sum(copies))
---
+++ AOC2023/day5/solution.m
@@ -0,0 +1,85 @@
+clear all
+filename    = 'input.txt';
+S           = readlines(filename);
+seeds       = str2double(strtrim(S(1).extractAfter(':')).split(' '));
+breaks      = find(S == '');
+breaks      = [breaks;numel(S)+1];
+
+for idx = 1:numel(breaks)-1 % cycle layers
+    map_set    = str2double(strtrim(S(breaks(idx)+2:breaks(idx+1)-1)).split(' '));
+    map_set(:,3) = map_set(:,3) - 1;
+    map_cell{idx} = map_set; % record transformations in each layer
+end
+
+%% part 1
+minloc = 10e20;
+for idx = 1:numel(seeds)
+    loc = Seed2Loc(seeds(idx),map_cell);
+    if loc < minloc
+        minloc = loc;
+    end
+end
+fprintf('part 1 = %10d\n',minloc)
+
+%% part 2
+current_ranges = [seeds(1:2:end) (seeds(1:2:end)+seeds(2:2:end)-1)]; %seed intervals
+current_ranges = sort(current_ranges(:))'; % convert to row
+for idx = 1:numel(breaks)-1 % cycle layers
+   %% insert each valid map interval limit into the ranges
+   map_set       = map_cell{idx};
+   set_intervals = [map_set(:,2), map_set(:,2)+map_set(:,3)]; % make intervals
+   set_intervals = sort(set_intervals(:))'; % convert to row
+   for jdx = 1:numel(set_intervals) 
+       current_ranges = insert_limit(current_ranges,set_intervals(jdx),jdx);
+   end
+   %% map layer tranformation
+   for jdx = 1:numel(current_ranges)
+       current_ranges(jdx) = Convert_layer(current_ranges(jdx),map_set);
+   end
+   current_ranges = sort(current_ranges(:))'; % sort new intervals
+end
+fprintf('part 2 = %10d\n',min(current_ranges(:,1)))
+
+
+%% functions
+
+function [ranges] = insert_limit(ranges,lim,lim_idx)
+    if mod(lim_idx,2) == 1 % left limit
+        flg = 1;
+    else   % right limit
+        flg = 2;
+    end
+    while flg <= numel(ranges)-1
+        if ((mod(lim_idx,2) ~= mod(flg,2)) & (mod(lim_idx,2) == 0) )& ranges(flg) < lim & ranges(flg+1) > lim % inserting left limit
+           ranges = [ranges(1:flg) lim lim+1 ranges(flg+1:end)];
+           flg = flg + 3;
+        elseif ((mod(lim_idx,2) ~= mod(flg,2)) & (mod(lim_idx,2) ==1) )& ranges(flg) > lim & ranges(flg-1) < lim % inserting right limit
+           ranges = [ranges(1:flg-1) lim lim+1 ranges(flg:end)];
+           flg = flg + 3;
+        else
+            flg = flg + 1;
+        end
+    end
+end
+function [loc] = Seed2Loc(seed,map_cell)
+    maps       = zeros([8,1]);
+    maps(1)    = seed;
+    for idx = 1:numel(map_cell)
+        map_set     = map_cell{idx};
+        maps(idx+1) = maps(idx);
+        maps(idx+1) = Convert_layer(maps(idx+1),map_set);
+    end
+    loc = maps(end);
+end
+
+
+function [out] = Convert_layer(inp,map_set)
+    out = inp; % set output = input
+    for jdx = 1:height(map_set) % cycle maps
+        if (map_set(jdx,2) <= inp) & (inp <= (map_set(jdx,2)+map_set(jdx,3))) % if source in range
+            out = map_set(jdx,1) + inp - map_set(jdx,2); % tranform to destination
+            break
+        end
+    end
+end
+
